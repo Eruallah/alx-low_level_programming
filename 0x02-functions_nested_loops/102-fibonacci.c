762f4afffb27eb1267b5d9689df189561ab821aa
Eruallah <abdullaholayiwola5@gmail.com>
2022-09-13 21:04:40 -0700

    uda fiber

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 
 /**
- * main - block to be executed
- * Description: computes
+ * main - block to execute
+ * Desciption: computes and prints
  * Return: 0
  */
+
 int main(void)
+
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
 
-	printf("%lu", bef);
+	int i = 0;
 
-	for (i = 1; i < 91; i++)
+	long int a = 0, b = 1, next;
+
+
+
+	while (i < 50)
+
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
 
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		next = a + b;
+
+		a = b;
+
+		b = next;
+
+		printf("%lu", next);
+
+
+
+		if (i < 49)
+
+		{
+
+			printf(", ");
+
+		}
+
+		i++;
+
 	}
-	printf("\n");
+
+	putchar('\n');
+
+
+
 	return (0);
+
 }
